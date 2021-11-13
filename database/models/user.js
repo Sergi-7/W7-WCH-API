7d399c9654a258ab6a163766162947238fd55e90
Sergi Petit <sergi_hak_7@hotmail.com>
2021-11-13 14:01:43 +0100

    Added model User to database models

---
+++ database/models/user.js
@@ -0,0 +1,34 @@
+const { Schema, model, Types } = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  friends: {
+    type: [Types.ObjectId],
+    ref: "Users",
+  },
+  enemies: {
+    type: [Types.ObjectId],
+    ref: "Users",
+  },
+  photo: {
+    type: String,
+  },
+  bio: {
+    type: String,
+  },
+});
+
+const User = model("User", userSchema, "Users");
+
+module.exports = User;
